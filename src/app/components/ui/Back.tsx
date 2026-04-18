b3493ae318bc725425595be805a2caa4b7d7dbb4
EWHERHE AKPESIR <ewherheakpesiri@gmail.com>
2026-04-18 17:03:45 +0100

    fixed everything

--- src/app/components/ui/Back.tsx
+++ src/app/components/ui/Back.tsx
@@ -15,7 +15,7 @@ const Back = () => {
       className="flex cursor-pointer items-center space-x-2 text-gray-700 hover:text-gray-900 transition-colors duration-200 z-50 absolute top-10 left-4"
     >
       <BsArrowLeft size={22} color="black" />
-      <span className="text-white">Back</span>
+      <span className="text-white cursor-pointer">Back</span>
     </div>
   );
 };
